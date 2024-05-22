2c634f2589558154ca68bc89e4d12f9733548521
SShipon <nxshipon@gmail.com>
2024-05-22 15:00:48 +0100

    crate order api add

---
+++ src/app/order/order.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import { orderServices } from './order.service'
+import { orderValidationSchema } from './order.validation'
+
+const createOrder = async (req: Request, res: Response) => {
+  try {
+    const data = req.body
+    const order = orderValidationSchema.parse(data)
+    const result = await orderServices.createOrder(order)
+    res.json({
+      success: true,
+      message: 'Order created successfully!',
+      data: result,
+    })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    if (err.name === 'ZodError') {
+      return res.status(500).json({
+        success: false,
+        message: err,
+      })
+    }
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    })
+  }
+}
+
+
+export const orderControllers = {
+  createOrder,
+ 
+}
---
+++ src/app/order/order.route.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import { orderControllers } from './order.controller'
+
+const router = express.Router()
+
+router.post('/', orderControllers.createOrder)
+
+
+export const orderRouter = router
