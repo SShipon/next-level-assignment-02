039b3a233a04cea33f74686295fc10363ae0b493
SShipon <nxshipon@gmail.com>
2024-05-22 23:47:23 +0100

    all comment

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,11 +1,13 @@
 import { TProduct } from './product.interface'
 import { Product } from './product.model'
 
+// crate post add single product
 const createProduct = async (payLoad: TProduct) => {
   const result = await Product.create(payLoad)
   return result
 }
 
+// get all product api
 const getAllProducts = async (searchTerm: unknown) => {
   if (typeof searchTerm === 'string') {
     const result = Product.find({ $text: { $search: searchTerm } })
@@ -15,12 +17,12 @@ const getAllProducts = async (searchTerm: unknown) => {
   return result
 }
 
-
+// crate single id details 
 const getProductById = async (id: string) => {
   const result = await Product.findById(id)
   return result
 }
-
+// crate data updated 
 const updateProduct = async (id: string, updatedProduct: TProduct) => {
   const result = await Product.findByIdAndUpdate(
     id,
@@ -33,6 +35,9 @@ const updateProduct = async (id: string, updatedProduct: TProduct) => {
   )
   return result
 }
+
+
+// data deleted 
 const deleteProduct = async (id: string) => {
   const result = await Product.findByIdAndDelete(id)
   return result
